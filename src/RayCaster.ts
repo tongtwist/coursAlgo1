3e03b7903ce81667dead773fc212e534f58fbb26
Tongtwist <arnaud.dussart@gmail.com>
2021-12-02 22:21:47 +0100

    comments RayCaster interfaces

--- src/RayCaster.ts
+++ src/RayCaster.ts
@@ -1,3 +1,8 @@
+/**
+ * @module
+ * Raycaster interfaces
+ */
+
 import type { IGrid } from "./Grid"
 import type {
 	IRay,
@@ -5,13 +10,61 @@ import type {
 } from "./Rays"
 
 
+/**
+ * Interface of a configuration object given to the constructor of any class that implement [[IRayCaster]]
+ *
+ * @export
+ * @interface IRayCasterConfig
+ */
 export interface IRayCasterConfig {
+	/**
+	 * IGrid object in which casting rays
+	 *
+	 * @type {IGrid}
+	 * @memberof IRayCasterConfig
+	 */
 	readonly grid: IGrid
+
+	/**
+	 * Irays object owning rays to cast
+	 *
+	 * @type {IRays}
+	 * @memberof IRayCasterConfig
+	 */
 	readonly rays: IRays
 }
 
+/**
+ * Interface of any ray caster object
+ *
+ * @export
+ * @interface IRayCaster
+ */
 export interface IRayCaster {
+	/**
+	 * Raycaster property owning the group of rays it cast
+	 *
+	 * @type {IRays}
+	 * @memberof IRayCaster
+	 */
 	readonly rays: IRays
+
+	/**
+	 * Method to cast one `ray` from (`x`,`y`) position
+	 *
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {IRay} ray
+	 * @memberof IRayCaster
+	 */
 	cast (x: number, y: number, ray: IRay): void
+
+	/**
+	 * Method that cast all rays of the inner `rays` property from (`x`,`y`) position
+	 *
+	 * @param {number} x
+	 * @param {number} y
+	 * @memberof IRayCaster
+	 */
 	castAll (x: number, y: number): void
 }
